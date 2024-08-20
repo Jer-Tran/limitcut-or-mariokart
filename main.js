4ecfd1232ab5b103f192d7fbdcfff162b744ec6a
Jer-Tran <jeremy.tran83@gmail.com>
2024-08-20 16:46:26 +1000

    presentation touch ups

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 var _mechs = []
+var _solved = false
 var _isLimitCut = false
 
 function NextMech() {
+    _next.style.display = "none"
     const index = Math.floor(Math.random() * _mechs.length)
     SetMech(_mechs[index])
 }
@@ -12,15 +14,50 @@ function SetMech(mech) {
     _name.innerText = mech.name
     _desc.innerText = mech.desc
     _image.src = mech.img
+    _isLimitCut = mech.limitcut
+    _remark.innerText = mech.remark
 }
 
 function PressLimitCut() {
-    console.log("limit cut")
+    if (_solved) {return}
+    _next.style.display = "inline-block"
+    if (_isLimitCut) {
+        Correct()
+    }
+    else {
+        Incorrect()
+    }
+    
 }
 
 function PressMarioKart() {
-    console.log("mario kart")
-    console.log(_mechs)
+    if (_solved) {return}
+    _next.style.display = "inline-block"
+    if (!_isLimitCut) {
+        Correct()
+    }
+    else {
+        Incorrect()
+    }
+}
+
+function GetMechType() {
+    if (_isLimitCut) {
+        return "Limit Cut"
+    }
+    return "Mario Kart"
+}
+
+function Correct() {
+    if (_remark.innerText != "") {return}
+
+    _remark.innerText = "Yes, it is a " + GetMechType()
+}
+
+function Incorrect() {
+    if (_remark.innerText != "") {return}
+
+    _remark.innerText = "No, it is actually a " + GetMechType()
 }
 
 function GetMechs() {
@@ -29,15 +66,21 @@ function GetMechs() {
 
         document.getElementById("limitcut").onclick = PressLimitCut
         document.getElementById("mariokart").onclick = PressMarioKart
-        document.getElementById("next").onclick = NextMech
+        _next.onclick = NextMech
 
         NextMech()
     }
     ).catch((error) => { console.log(error) })
 }
 
+function ToggleDarkMode() {
+    // lmao no
+}
+
 const _name = document.getElementById("name")
 const _desc = document.getElementById("desc")
 const _image = document.getElementById("image")
+const _remark = document.getElementById("remark")
+const _next = document.getElementById("result")
 
 GetMechs()
